dbfa72933a69c7e9c9fd8cfededce7689a10ec42
Bitheen <richardgomes098@gmail.com>
2023-09-28 17:05:12 -0300

    exercicios atuais

---
+++ exercio1.py
@@ -0,0 +1,5 @@
+numero1 = int(input("numero1: "))
+numero2 = int(input("numero2: "))
+numero3 = int(input("numero3: ")) 
+media = (numero1 + numero2 + numero3) / 3
+print(media) 
---
+++ exercio2.py
@@ -0,0 +1,8 @@
+anoNascimento = int(input("Ano de nascimento: "))
+anoAtual = int(input("Ano atual: "))
+
+idadeAtual = anoAtual - anoNascimento
+idadeDefinida = 2050 - anoNascimento
+
+print(f"No ano 2050 voce terá: {idadeDefinida}  ")
+print(f"No atual voce tera {idadeAtual} ta velho em")
