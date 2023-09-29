b3a444b7997feacccb315d999f21f9cfd06095ae
Bitheen <richardgomes098@gmail.com>
2023-09-29 15:41:06 -0300

    Co-authored-by: Gabi <gabriellesote@users.noreply.github.com>

---
+++ exercio3.py
@@ -0,0 +1,7 @@
+n1 = float(input("valor do dolar: "))
+print(f"Atualmente a cotação do dolar está na média de {n1} reais")
+
+n2 = float(input("Valor na carteira digital: "))
+converter = n2 / n1
+
+print(f"o valor na carteira é de R${converter} seu pobre")
