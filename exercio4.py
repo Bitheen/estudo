e65e9a0a3471a962483e99d16969d3f00e9cac41
Bitheen <richardgomes098@gmail.com>
2023-10-03 15:45:12 -0300

    exerciopy

---
+++ exercio4.py
@@ -0,0 +1,5 @@
+diagonalmaior = float(input("diagonal maior"))
+diagonalmenor = float(input("diagonal menor"))
+
+res = (diagonalmaior * diagonalmenor)/2
+print(res)
---
+++ exercio5.py
@@ -0,0 +1,3 @@
+celcios = float(input("valor celcios: " ))
+fahrenheit = (celcios*1.8) + 32
+print(fahrenheit)
